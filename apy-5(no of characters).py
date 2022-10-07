182079d25b3e71f57bb08e5d697e84b22dc5150f
Likith Kiran Singh Bondili <113082870+likith-kiran-singh@users.noreply.github.com>
2022-10-07 19:51:25 +0530

    Update apy-5(no of characters).py

--- apy-5(no of characters).py
+++ apy-5(no of characters).py
@@ -8,3 +8,16 @@ else:
     for i in s:
         d[i]=s.count(i)
     print(d)
+    
+    
+"""
+Output:
+i.  Enter a string:hello
+    {'h': 1, 'e': 1, 'l': 2, 'o': 1}
+ii. Enter a string:likith kiran singh
+    {'l': 1, 'i': 4, 'k': 2, 't': 1, 'h': 2, ' ': 2, 'r': 1, 'a': 1, 'n': 2, 's': 1, 'g': 1}   
+iii. Enter a string:21bq1a4220
+    {'2': 3, '1': 2, 'b': 1, 'q': 1, 'a': 1, '4': 1, '0': 1}
+iv. Enter a string:56444
+    Enter characters only
+"""
